9f226dc2ea3a178edc46aadb6034992abd42f188
Anton Reshetov <reshetov.art@gmail.com>
2019-01-07 14:20:12 +0300

    refactor: template via "table rule" #5

--- src/components/templates/EmailTemplate1.vue
+++ src/components/templates/EmailTemplate1.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <table
+      cellspacing="0"
+      cellpadding="0"
+      border="0"
       style="font-family: Arial, Helvetica, sans-serif; line-height: 1.5"
       :style="{
         fontSize: options.font.size + 'px',
@@ -8,6 +11,7 @@
       }"
     >
       <tbody>
+        <!-- Avatar column -->
         <tr>
           <td style="vertical-align: top;">
             <img
@@ -30,61 +34,111 @@
           }"
             ></div>
           </td>
+          <!-- Info column -->
           <td style="vertical-align: top;">
-            <span style="display: block;" :style="{fontSize: `${options.font.size + 2}px` }">
-              <span
-                style="font-weight: 600"
-                :style="{color: options.color.mainPreview || options.color.main}"
-              >{{ mainFields[0].value }}</span>
-            </span>
-            <span style="display: block; margin-bottom: 10px;">
-              <span>{{ mainFields[1].value }}</span>
-              <span v-if="mainFields[2].value">
-                <span v-if="options.separator !== 'br'">&nbsp;{{ options.separator }}&nbsp;</span>
-                <br v-else>
-              </span>
-              <span>{{ mainFields[2].value }}</span>
-            </span>
-            <template v-for="item in otherFields">
-              <span v-if="item.value" style="display: block;" :key="item.id">
-                <span
-                  style="font-weight: 600;"
-                  :style="{color: options.color.secondaryPreview || options.color.secondary}"
-                >{{ item.name }}:</span>
-                <a
-                  v-if="item.type === 'link'"
-                  :href="formatLink(item.value)"
-                  style="text-decoration: none; color: inherit; padding-left: 5px;"
-                >{{ item.value }}</a>
-                <a
-                  v-if="item.type === 'email'"
-                  :href="`mailto:${item.value}`"
-                  style="text-decoration: none; color: inherit; padding-left: 5px;"
-                >{{ item.value }}</a>
-                <span v-if="item.type === 'text'" style="padding-left: 5px;">{{ item.value }}</span>
-              </span>
-            </template>
-            <a
-              style="display: inline-block; font-size: 0;"
-              v-for="item in socials.installed"
-              :key="item.name"
-              :href="formatLink(item.link)"
+            <table
+              cellspacing="0"
+              cellpadding="0"
+              border="0"
+              :style="{fontSize: options.font.size + 'px'}"
             >
-              <span
-                style="width: 20px; height:20px; display: inline-block; padding: 4px; border-radius: 3px; box-sizing: border-box; margin: 10px 3px 0 0;"
-                :style="{
-                  backgroundColor: options.color.mainPreview || options.color.main,
-                  backgroundImage: `url(https://s3.eu-central-1.amazonaws.com/mysigmail/icons/${item.icon}.png)`,
-                  backgroundSize: '12px',
-                  backgroundPosition: 'center center',
-                  backgroundRepeat: 'no-repeat'
-                }"
-              ></span>
-            </a>
+              <tbody>
+                <!-- Name and job fields -->
+                <tr>
+                  <td>
+                    <span
+                      style="font-weight: 600"
+                      :style="{
+                        fontSize: `${options.font.size + 2}px`,
+                        color: options.color.mainPreview || options.color.main
+                      }"
+                    >{{ mainFields[0].value }}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <span>{{ mainFields[1].value }}</span>
+                    <span
+                      v-if="mainFields[2].value && options.separator !== 'br'"
+                    >&nbsp;{{ options.separator }}&nbsp;</span>
+                    <span>{{ mainFields[2].value }}</span>
+                  </td>
+                </tr>
+                <!-- Other fields -->
+                <tr>
+                  <td>
+                    <table
+                      cellspacing="0"
+                      cellpadding="0"
+                      border="0"
+                      style="margin-top: 5px;"
+                      :style="{fontSize: options.font.size + 'px'}"
+                    >
+                      <template v-for="item in otherFields">
+                        <tr :key="item.name">
+                          <td>
+                            <span
+                              style="font-weight: 600;"
+                              :style="{color: options.color.secondaryPreview || options.color.secondary}"
+                            >{{ item.name }}:&nbsp;</span>
+                            <a
+                              v-if="item.type === 'link'"
+                              :href="formatLink(item.value)"
+                              style="text-decoration: none; color: inherit;"
+                            >{{ item.value }}</a>
+                            <a
+                              v-if="item.type === 'email'"
+                              :href="`mailto:${item.value}`"
+                              style="text-decoration: none; color: inherit;"
+                            >{{ item.value }}</a>
+                            <span v-if="item.type === 'text'">{{ item.value }}</span>
+                          </td>
+                        </tr>
+                      </template>
+                    </table>
+                  </td>
+                </tr>
+                <!-- Social icons -->
+                <tr>
+                  <td>
+                    <table cellspacing="0" cellpadding="0" border="0" style=" margin-top: 5px;">
+                      <tbody>
+                        <tr>
+                          <td
+                            v-for="item in socials.installed"
+                            :key="item.name"
+                            align="center"
+                            style="padding-right:3px;"
+                          >
+                            <a :href="formatLink(item.link)">
+                              <div
+                                style="width: 20px; height:20px; border-radius: 3px; font-size: 12px; line-height: 18px;"
+                                :style="{
+                                  backgroundColor: options.color.mainPreview || options.color.main,
+                                }"
+                              >
+                                <img
+                                  width="12px"
+                                  height="12px"
+                                  :src="`https://s3.eu-central-1.amazonaws.com/mysigmail/icons/${item.icon}.png`"
+                                  alt=""
+                                  style="vertical-align: middle;"
+                                >
+                              </div>
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </td>
         </tr>
       </tbody>
     </table>
+    <!-- Addons -->
     <table
       v-if="isAdded('disclaimer')"
       :style="{
