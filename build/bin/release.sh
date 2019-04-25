7a8698e06a3218ddbcd81f963beaa50c0cd6d486
Anton Reshetov <reshetov.art@gmail.com>
2019-04-25 11:20:32 +0300

    fix: release commit msg

--- build/bin/release.sh
+++ build/bin/release.sh
@@ -9,7 +9,7 @@ read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  npm version $VERSION -m "release $VERSION"
+  npm version $VERSION -m "build: release $VERSION"
   npm run build
   git push origin master
 fi
