b79d7372944bff19a6abedc508c8fe279349f0b9
Anton Reshetov <reshetov.art@gmail.com>
2025-09-15 12:45:50 +0300

    build: update configuration and dependencies for auto-import and vue components

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+    }),
+    Components({
+      dts: true,
+      directoryAsNamespace: true,
+      collapseSamePrefixes: true,
+      types: [
+        {
+          from: 'vue-router',
+          names: ['RouterLink', 'RouterView'],
+        },
+      ],
+    }),
+  ],
 })
