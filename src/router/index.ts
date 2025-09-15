671ed8526df05a1732bdfc7f3ff185e1d1bb7cb6
Anton Reshetov <reshetov.art@gmail.com>
2025-09-15 15:34:16 +0300

    chore: correct title casing for templates route

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,7 +33,7 @@ const routes = [
     path: '/templates',
     component: () => import('@/views/Templates.vue'),
     meta: {
-      title: 'templates',
+      title: 'Templates',
     },
   },
   {
